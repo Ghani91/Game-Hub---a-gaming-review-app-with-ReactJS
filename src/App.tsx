f52dea9373935ba45877d2a5cb0b5df9f9822101
Usman Ghani <usman.ghaniee@gmail.com>
2024-09-02 13:43:19 +0500

    adding a heading element for the page .... a test case to know how to use Chakra UI ... a very good UI library & i have started loving as i have started doing it .....

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Heading } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <h1>Welcome to Game Hub - a wholecome approach for gaming world</h1>;
+  return (
+    <>
+      <Heading
+        as="h1"
+        color="green"
+        textAlign="center"
+        p="20px"
+        border="1px solid grey"
+        borderRadius="10px"
+        m="10px"
+      >
+        Welcome to Gaming Hub - a true friend of gamers!!!
+      </Heading>
+    </>
+  );
 }
 
 export default App;
