20e085e71e246c0e482f62d461c08c89d9f27346
Usman Ghani <usman.ghaniee@gmail.com>
2024-09-03 22:16:48 +0500

    implementing the abort controller ...

--- src/services/useGames.tsx
+++ src/services/useGames.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "./api-client";
+import { CanceledError } from "axios";
 interface Game {
   id: number;
   name: string;
@@ -13,17 +14,22 @@ const useGames = () => {
   const [errors, setErrors] = useState("");
   const [isLoading, setLoading] = useState(false);
   useEffect(() => {
+    const controller = new AbortController();
     setLoading(true);
     apiClient
-      .get<FetchGames>("/games")
+      .get<FetchGames>("/games", { signal: controller.signal })
       .then((resp) => {
         setGames(resp.data.results);
         setLoading(false);
       })
       .catch((errs) => {
+        if (errs instanceof CanceledError) return;
         setErrors(errs.message);
         setLoading(false);
       });
+    return () => {
+      controller.abort();
+    };
   }, []);
   return { games, errors, isLoading };
 };
