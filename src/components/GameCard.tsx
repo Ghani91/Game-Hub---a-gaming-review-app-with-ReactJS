8a80ad767fe18d562612bdd325c27685042acc30
Usman Ghani <usman.ghaniee@gmail.com>
2024-09-04 00:09:49 +0500

    implementing the icons of the supported platform ... with a separate component & displaying icons dynamically ...

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../services/useGames";
+import PlatformIconList from "./PlatformIconList";
 interface Props {
   game: Game;
 }
@@ -11,6 +12,7 @@ const GameCard = ({ game }: Props) => {
         <Heading fontSize="2xl" textAlign={"center"}>
           {game.name}
         </Heading>
+        <PlatformIconList platforms={game.platforms.map((p) => p.platform)} />
       </CardBody>
     </Card>
   );
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,51 @@
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../services/useGames";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  platforms.forEach((p) => console.log(p.slug));
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation5: FaPlaystation,
+    playstation3: FaPlaystation,
+    playstation4: FaPlaystation,
+    "xbox-one": FaXbox,
+    xbox360: FaXbox,
+    "xbox-series-x": FaXbox,
+    "nintendo-switch": SiNintendo,
+    macos: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack
+      marginY={5}
+      justifyItems={"center"}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} boxSize={8} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
