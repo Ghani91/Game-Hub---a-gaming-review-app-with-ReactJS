79590be3c3a3eb9eb360c12472b8c7a44b03956e
Usman Ghani <usman.ghaniee@gmail.com>
2024-09-20 12:56:27 +0500

    implementing the platform lists dynamically ...

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenre";
+import PlatformsMenu from "./components/PlatformsMenu";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -28,6 +29,7 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main">
+          <PlatformsMenu />
           <GameGrid selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>
