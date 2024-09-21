04a8e30639a0b33bdc1f755689a9e182d249ecf1
Usman Ghani <usman.ghaniee@gmail.com>
2024-09-21 09:51:46 +0500

    implementing the searchbar for the website

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,12 @@
-import { Heading, HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => (
-  <HStack justifyContent="space-between" padding="10px">
+  <HStack padding="10px">
     <Image borderRadius="full" boxSize="60px" src={logo} alt="Dan Abramov" />
-    <Heading as="h1" textTransform={"capitalize"} color="gray.600">
-      {" "}
-      Gaming Hub - a true friend of gamers
-    </Heading>
+    <SearchInput />
     <ColorModeSwitch />
   </HStack>
 );
